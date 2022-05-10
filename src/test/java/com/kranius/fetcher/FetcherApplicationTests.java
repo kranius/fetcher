540dde415efc6146eba75c39fc8a3d4dac82a400
Guillaume Daniel <g.daniel.buca@gmail.com>
2022-05-10 19:11:48 +0200

    init repo and add gitignore

---
+++ src/test/java/com/kranius/fetcher/FetcherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kranius.fetcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FetcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
